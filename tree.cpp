6c1c92208219ecdc79405ab5e4b0a4f54797b156
KuzminStanislav <a663ve@gmail.com>
2025-02-19 19:22:15 +0400

    helping class

--- tree.cpp
+++ tree.cpp
@@ -1,5 +1,25 @@
 ﻿#include <iostream>
 
+size_t lcg() {
+    static size_t x = 0;
+    x = (1021 * x * 24631) % 116640;
+    return x;
+}
+
+struct Node {
+    T data;
+    Node* left;
+    Node* right;
+
+    Node(const T& value): data(value), left(nullptr), right(nullptr) {}
+};
+
+template <typename T>
+class BinaryTree {
+private:
+    Node* root;
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
